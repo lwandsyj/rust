d94223c06ef29fa6fa44ef30ccf0959c9cb64a15
li <lwaisyj@163.com>
2020-11-06 18:17:12 +0800

    所有权

--- code/learn1/src/main.rs
+++ code/learn1/src/main.rs
@@ -1,21 +1,15 @@
-struct Point{
-    x:i32,
-    y:i32
-}
-impl Point{
-    fn new(x:i32,y:i32)->Point{
-        Point{x,y}
-    }
-    fn get_y(&mut self)->i32{
-        self.y=self.y+4;
-        return self.y;
-    }
-}
+
 
 fn main() {
-   let add =|a:i32,b:i32| a+b;
-   let val = add(2,3);
-   println!("{}",val);
+   let  a="hello";
+   let mut b=a;
+   b="world";
+   println!("{}",a);
+    
+   println!("{}",b);
 }
 
+fn show(b:&str){
+    println!("{}",b);
+}
 

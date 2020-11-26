da3ec9e5a59f8780110672deacf0ccf3bc86071c
li <lwaisyj@163.com>
2020-11-26 18:23:30 +0800

    rust

--- lear2/src/main.rs
+++ lear2/src/main.rs
@@ -1,8 +1,7 @@
 fn main(){
-    let s = String::from("hello");
-    let x = s.as_str();
-    let d = x.to_string();
-    println!("{}",s);
-    println!("{}",x);
-    println!("{}",d);
+  
+}
+
+ fn maximum_gap(nums: Vec<i32>) -> i32 {
+        
 }

779a6083ecd67c18ebd869e09e44a2656b194980
li <lwaisyj@163.com>
2020-11-27 13:47:16 +0800

    算法

--- lear2/src/main.rs
+++ lear2/src/main.rs
@@ -1,7 +1,23 @@
 fn main(){
-  
+    let mut arr =vec![1,3,100];
+    maximum_gap(arr);
 }
 
- fn maximum_gap(nums: Vec<i32>) -> i32 {
-        
+ fn maximum_gap(mut nums: Vec<i32>) -> i32 {
+    let len=nums.len();
+    if len < 2 {
+        return 0;
+    }
+    nums.sort();
+    let mut max =0;
+    for i in 0..len-1 {
+        let last = nums[(i+1)];
+        let current =nums[i];
+        let tmp = last-current;
+        if max < tmp {
+            max=tmp;
+        }
+    }
+    println!("{:?}",max);
+    return max;
 }

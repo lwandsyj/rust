fc666fc39e1fa87e7a91f3115fed686c742cbddf
li <lwaisyj@163.com>
2020-12-21 13:35:37 +0800

    vec string

--- learn1/src/main.rs
+++ learn1/src/main.rs
@@ -1,6 +1,13 @@
-mod foo;
-use foo::bar::Bar;
-
 fn main() {
-    Bar::hello();
+   let a =String::from("hello");
+   let b=String::from("world");
+   for i in a.chars(){
+       println!("{}",i);
+   }
+   let len=a.len();
+   for i in 0..len{
+       let item=a.chars().into_iter().nth(i);
+       println!("{:?}",item.unwrap());
+   }
+
 }
